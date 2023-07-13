281e2c627e9b92a670d94f76008a7047c172b68d
Varu98 <vardaan@wisdomtreeventures.com>
2023-07-13 11:39:58 +0530

    add : pure class

--- intro-to-react/books-lisiting/src/components/BookItem.tsx
+++ intro-to-react/books-lisiting/src/components/BookItem.tsx
@@ -1,13 +1,26 @@
+import { PureComponent, ReactNode } from "react";
 import { BookItemProps } from "../types/book";
 
-const BookItem = ({ book }: BookItemProps) => {
-  return (
-    <li className="book-item">
-      <p className="book-year">{book.year}</p>
-      <h3 className="book-title">{book.title}</h3>
-      <p>{book.author}</p>
-    </li>
-  );
-};
+// const BookItem = ({ book }: BookItemProps) => {
+//   return (
+//     <li className="book-item">
+//       <p className="book-year">{book.year}</p>
+//       <h3 className="book-title">{book.title}</h3>
+//       <p>{book.author}</p>
+//     </li>
+//   );
+// };
 
+class BookItem extends PureComponent<BookItemProps> {
+  render() {
+    const { book } = this.props;
+    return (
+      <li className="book-item">
+        <p className="book-year">{book.year}</p>
+        <h3 className="book-title">{book.title}</h3>
+        <p>{book.author}</p>
+      </li>
+    );
+  }
+}
 export default BookItem;
