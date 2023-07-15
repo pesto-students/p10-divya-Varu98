934ef6d462b5aaa561410fa2587948d6ac6f3563
Varu <vardaan.agarwal.98@gmail.com>
2023-07-15 15:16:23 +0530

    add : icon to different file

--- Week-13/url-shortener/src/components/Input.jsx
+++ Week-13/url-shortener/src/components/Input.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { usePostUrl } from "../context/UrlContext";
+import Icon from "./Icon";
 
 const Input = () => {
   const { data, setData, shortenUrl } = usePostUrl();
@@ -26,21 +27,7 @@ const Input = () => {
       </label>
       <div className="relative">
         <div className="absolute max-sm:bottom-12 inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-          <svg
-            aria-hidden="true"
-            className="w-5 h-5 text-gray-500 dark:text-gray-400"
-            fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            ></path>
-          </svg>
+          <Icon />
         </div>
         <input
           type="url"
