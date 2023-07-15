2cdbf16c9e0c1736da47c2ec8f96926a2081d35d
Varu <vardaan.agarwal.98@gmail.com>
2023-07-15 15:18:58 +0530

    fix : add layout to higher level

--- Week-13/url-shortener/src/App.js
+++ Week-13/url-shortener/src/App.js
@@ -3,17 +3,20 @@ import AboutUs from "./components/AboutUs";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./components/Contact";
+import Layout from "./components/Layout";
 
 const App = () => {
   return (
     <>
-      <div className="w-full px-4 md:px-0 md:max-w-[70vw] mx-auto">
-        <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/about"} element={<AboutUs />} />
-          <Route path={"/contact"} element={<Contact />} />
-        </Routes>
-      </div>
+      <Layout>
+        <div className="w-full px-4 md:px-0 md:max-w-[70vw] mx-auto">
+          <Routes>
+            <Route path={"/"} element={<Home />} />
+            <Route path={"/about"} element={<AboutUs />} />
+            <Route path={"/contact"} element={<Contact />} />
+          </Routes>
+        </div>
+      </Layout>
       {/* <AboutUs /> */}
     </>
   );
--- Week-13/url-shortener/src/pages/Home.jsx
+++ Week-13/url-shortener/src/pages/Home.jsx
@@ -2,16 +2,13 @@ import React from "react";
 import Header from "../components/Header";
 import Input from "../components/Input";
 import Output from "../components/Output";
-import Layout from "../components/Layout";
 
 const Home = () => {
   return (
     <div className="mt-12">
-      <Layout>
         <Header />
         <Input />
         <Output />
-      </Layout>
     </div>
   );
 };
