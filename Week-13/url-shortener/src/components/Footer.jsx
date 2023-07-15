4cb81078cc5632d01585a445f7b800e91417f1b2
Varu <vardaan.agarwal.98@gmail.com>
2023-07-15 15:08:01 +0530

    refactor : Footer Links

--- Week-13/url-shortener/src/components/Footer.jsx
+++ Week-13/url-shortener/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Footer = () => {
+  const links = ["About", "Privacy Policy", "Licensing", "Contact"];
   return (
     <div className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
@@ -12,26 +13,13 @@ const Footer = () => {
             </span>
           </button>
           <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
-            <li>
-              <a href="/" className="mr-4 hover:underline md:mr-6 ">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="/" className="mr-4 hover:underline md:mr-6">
-                Privacy Policy
-              </a>
-            </li>
-            <li>
-              <a href="/" className="mr-4 hover:underline md:mr-6 ">
-                Licensing
-              </a>
-            </li>
-            <li>
-              <a href="/" className="hover:underline">
-                Contact
-              </a>
-            </li>
+            {links.map((link) => (
+              <li>
+                <a href="/" className="mr-4 hover:underline md:mr-6 ">
+                  {link}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
