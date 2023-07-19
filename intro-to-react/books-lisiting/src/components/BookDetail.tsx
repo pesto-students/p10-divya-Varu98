e57552cc389b45d4db5a1106947c91e57ee9313a
Varu <vardaan.agarwal.98@gmail.com>
2023-07-19 08:00:59 +0530

    add : state

--- intro-to-react/books-lisiting/src/components/BookDetail.tsx
+++ intro-to-react/books-lisiting/src/components/BookDetail.tsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { BookDetailProps } from "../types/book";
 
 const BookDetail = ({ book }: BookDetailProps) => {
+  const [showDescription, setShowDescription] = useState();
   return (
     <div>
       <h1>{book.title}</h1>
